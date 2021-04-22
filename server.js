3355aa9491f591a5d1220a5d394ace22803e1efa
Academy-of-Code <77977795+Academy-of-Code@users.noreply.github.com>
2021-04-21 20:48:11 -0400

    Update server.js

--- server.js
+++ server.js
@@ -12,44 +12,13 @@ const server = express()
 
 const wss = new Server({ server });
 
-const clientNameArr = ['John','Billy','Harold','Dillion','Dylan','Nemo','Car Guy','Computor Geek','NerdMan2000','xX_PinkUnicorns_Xx']
-
 wss.on('connection', (ws) => {
-  ws.isAlive = true
-  ws.on('pong', heartbeat);
-  
-  var clientName = clientNameArr[randomInt(0,clientNameArr.length)]
-  
-  console.log('Client *'+clientName+'* connected');
+  console.log('Client connected');
   ws.on('close', () => console.log('Client disconnected'));
-  ws.on('message', function incoming(message) {
-    console.log(clientName+': '+message)
-    sendToAllClients(clientName+': '+message)
-  });
 });
 
-const interval = setInterval(function ping() {
-  wss.clients.forEach(function each(ws) {
-    if(ws.isAlive === false) return ws.terminate();
-    
-    ws.isAlive = false;
-    ws.ping(noop);
-  });
-}, 30000);
-wss.on('close', function close() {
-  clearInterval(interval);
-});
-
-function noop() {}
-function heartbeat() {
-  this.isAlive = true;
-}
-function sendToAllClients(msg){
+setInterval(() => {
   wss.clients.forEach((client) => {
-    client.send(msg)
-    return(msg)
+    client.send(new Date().toTimeString());
   });
-}
-function randomInt(min, max) { // min and max included 
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+}, 1000);
