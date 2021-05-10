af5652b3b19678dcd4b1ad6f6c84493b69a54fcd
Academy-of-Code <77977795+Academy-of-Code@users.noreply.github.com>
2021-05-10 10:04:04 -0400

    Update server.js

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ wss.on('connection', (ws) => {
         }
       }
       if(match===false /* && cycles===games.length */){
-        games.push( {gameName:gameName,clicks:1,gameIndex:games.length} )
-        console.log('Match not found! Making index for that game!');
+        games.push( {gameName:gameName,clicks:1,gameIndex:games.length,displayName:msg.split('-')[2]} )
+        console.log('Match not found! Making index for '+msg.split('-')[2]);
       }
     }
   })
